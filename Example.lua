ec7a11ea5e30fa715592ad5c73e58768b4779d8c
thk-bin <160216488+thk-bin@users.noreply.github.com>
2024-03-20 16:01:35 +0700

    Update Example.lua

--- Example.lua
+++ Example.lua
@@ -1,27 +1,7 @@
-local library = loadstring(game:HttpGet('https://raw.githubusercontent.com/thk-bin/CUTTAY-BY-binhackerlo/main/source.lua'))()
+local Library = loadstring(game:HttpGet('https://raw.githubusercontent.com/thk-bin/CUTTAY-BY-binhackerlo/main/source.lua'))()
+Library.Theme = "Dark"
+local Flags = Library.Flags
 
-local Window = library:CreateWindow("CUTTAY HUB-Blox Fruit", "By binhackerlo (Version)", 10044538000)
-
-local Tab = Window:CreateTab("Tap")
-
-local Page = Tab:CreateFrame("Tap Raid")
-
-Button = Page:CreateButton("Tap Raid", "How To Fix code", function()
-CreateNotification("Title", "Description", function(value)
-if value == true then
-print(true)
-else
-print(false)
-end
-end)
-end)
-
-Toggle = Page:CreateToggle("Toggle", "Description", function(arg)
-Toggle:UpdateToggle("New Title", "New Description")
-print(arg)
-end)
-
-Bind = Page:CreateBind("Chip", "Flame","Ice","Quake","Light","Dark","Spider","Rumble","Magma","Buddha","Sand","Phoenix","Dough", function(arg)
-Bind:UpdateBind("New Title")
-print(arg)
-end)
+local Window = Library:Window({
+   Text = "CUTTAY HUB-Blox Fruit | By Binhackerlo (Version)"
+})
