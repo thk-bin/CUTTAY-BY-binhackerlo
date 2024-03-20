e187f3cdef20c5125ecd31788985e09430df49dc
thk-bin <160216488+thk-bin@users.noreply.github.com>
2024-03-20 15:33:21 +0700

    Update Example.lua

--- Example.lua
+++ Example.lua
@@ -5,3 +5,17 @@ local Window = library:CreateWindow("CUTTAY HUB-Blox Fruit", "By binhackerlo (Ve
 local Tab = Window:CreateTab("Tap Hub")
 
 local Page = Tab:CreateFrame("Raid-Material")
+
+Button = Page:CreateButton("Buy Chip", "", function()
+    CreateNotification("Title", "Description", function(value)
+    if value == true then
+    print(true)
+    else
+    print(false)
+    Bind = Page:CreateBind("Chip Fix", "{"Flame","Ice","Quake","Light","Dark","Spider","Rumble","Magma","Buddha","Sand","Phoenix","Dough"}", function(arg)
+        Bind:UpdateBind("New Title")
+        print(arg)
+        end)
+    end
+    end)
+    end)
